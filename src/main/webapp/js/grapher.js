fe5a53863d88b92b5f068e85e2439e772dc2b34b
ThisAster <338879@niuitmo.ru>
2022-11-21 22:06:41 +0300

    fix table.jsp; added localStorage; added clean

--- src/main/webapp/js/grapher.js
+++ src/main/webapp/js/grapher.js
@@ -4,100 +4,132 @@ const width = canvas.width;
 const height = canvas.height;
 const ctx = canvas.getContext("2d");
 let rValue = "";
+const rCacheKey = 'R';
 
-function runGrapher() {
+document.addEventListener("DOMContentLoaded", function () {
+    loadDataFromLocalStorage();
+    drawGraph();
+});
+
+function loadDataFromLocalStorage() {
+    const r = localStorage.getItem(rCacheKey);
+    if (r) {
+        setR(r);
+        const rCheckbox = document.getElementById(`r${r}`);
+        if(rCheckbox){
+            rCheckbox.checked = true;
+        }
+        return {rCheckbox};
+    }
+}
+
+function clean() {
+    if (localStorage.getItem(rCacheKey)) {
+        localStorage.clear();
+    }
+}
 
 
-    const FIGURE_COLOR = "#567efb99";
-    const POINT_COLOR = "#4A76FE99";
 
-    function drawGraph() {
-        ctx.font = "13px sans-serif";
-        ctx.fillStyle = "#FFF";
-        ctx.fillRect(0, 0, width, height);
+const FIGURE_COLOR = "#567efb99";
+const POINT_COLOR = "#4A76FE99";
 
-        ctx.fillStyle = FIGURE_COLOR;
-        // 1st quadrant rectangle
-        ctx.fillRect(width / 2, height / 3, width / 3, height / 6);
-        canvas.onmousemove = (e) => {
-            drawGraph();
+function drawGraph() {
+    ctx.font = "13px sans-serif";
+    ctx.fillStyle = "#FFF";
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = FIGURE_COLOR;
+    // 1st quadrant rectangle
+    ctx.fillRect(width / 2, height / 3, width / 3, height / 6);
+    canvas.onmousemove = (e) => {
+        drawGraph();
+        let isMainScreen = !document.URL.includes('x_coord');
+        if(isMainScreen){
             ctx.fillStyle = POINT_COLOR;
             ctx.beginPath();
             ctx.arc(e.offsetX, e.offsetY, 5, 0, Math.PI * 2);
             ctx.fill();
-        };
-
-        // 2nd quadrant triangle
-        ctx.beginPath();
-        ctx.moveTo(width / 2, height / 3);
-        ctx.lineTo((5 * width) / 10, height / 2);
-        ctx.lineTo(width / 3, height / 2);
-        ctx.fill();
-
-        ctx.beginPath();
-        ctx.moveTo(0, height / 2);
-        ctx.lineTo(width, height / 2);
-        ctx.lineTo(width - 10, height / 2 - 10);
-        ctx.moveTo(width, height / 2);
-        ctx.lineTo(width - 10, height / 2 + 10);
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(width / 2, height);
-        ctx.lineTo(width / 2, 0);
-        ctx.lineTo(width / 2 - 10, 10);
-        ctx.moveTo(width / 2, 0);
-        ctx.lineTo(width / 2 + 10, 10);
-        ctx.stroke();
-
-        // 3nd quadrant sector
-        ctx.beginPath();
-        ctx.arc(width / 2, height / 2, width / 3, -3.14, Math.PI / 2, true);
-        ctx.lineTo(width / 2, height / 2);
-        ctx.fill();
-
-        ctx.fillStyle = "#000";
-        const labels = ["-R", "-R/2", "", "R/2", "R"];
-
-        for (let i = 1; i < 6; i++) {
-            ctx.beginPath();
-            ctx.moveTo((i * width) / 6, height / 2 - 5);
-            ctx.lineTo((i * width) / 6, height / 2 + 5);
-            ctx.moveTo(width / 2 - 5, (i * height) / 6);
-            ctx.lineTo(width / 2 + 5, (i * height) / 6);
-            ctx.stroke();
-
-            ctx.textAlign = "center";
-            ctx.textBaseline = "bottom";
-            ctx.fillText(labels[i - 1], (i * width) / 6, height / 2 - 7);
-
-            ctx.textAlign = "left";
-            ctx.textBaseline = "middle";
-            ctx.fillText(labels[i - 1], width / 2 + 7, height - (i * height) / 6);
         }
+    };
 
+    // 2nd quadrant triangle
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height / 3);
+    ctx.lineTo((5 * width) / 10, height / 2);
+    ctx.lineTo(width / 3, height / 2);
+    ctx.fill();
 
-        POINTS.forEach((point, index) => {
-            const x = ((point.x / rValue) * width) / 3 + width / 2;
-            const y = ((-point.y / rValue) * height) / 3 + height / 2;
+    ctx.beginPath();
+    ctx.moveTo(0, height / 2);
+    ctx.lineTo(width, height / 2);
+    ctx.lineTo(width - 10, height / 2 - 10);
+    ctx.moveTo(width, height / 2);
+    ctx.lineTo(width - 10, height / 2 + 10);
+    ctx.stroke();
 
-            ctx.fillStyle = point.color;
-            ctx.beginPath();
-            ctx.arc(x, y, 5, 0, Math.PI * 2);
-            ctx.fill();
-            ctx.closePath();
-        });
+    ctx.beginPath();
+    ctx.moveTo(width / 2, height);
+    ctx.lineTo(width / 2, 0);
+    ctx.lineTo(width / 2 - 10, 10);
+    ctx.moveTo(width / 2, 0);
+    ctx.lineTo(width / 2 + 10, 10);
+    ctx.stroke();
+
+    // 3nd quadrant sector
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, width / 3, -3.14, Math.PI / 2, true);
+    ctx.lineTo(width / 2, height / 2);
+    ctx.fill();
+
+    ctx.fillStyle = "#000";
+    const labels = ["-R", "-R/2", "", "R/2", "R"];
+
+    for (let i = 1; i < 6; i++) {
+        ctx.beginPath();
+        ctx.moveTo((i * width) / 6, height / 2 - 5);
+        ctx.lineTo((i * width) / 6, height / 2 + 5);
+        ctx.moveTo(width / 2 - 5, (i * height) / 6);
+        ctx.lineTo(width / 2 + 5, (i * height) / 6);
+        ctx.stroke();
+
+        ctx.textAlign = "center";
+        ctx.textBaseline = "bottom";
+        ctx.fillText(labels[i - 1], (i * width) / 6, height / 2 - 7);
+
+        ctx.textAlign = "left";
+        ctx.textBaseline = "middle";
+        ctx.fillText(labels[i - 1], width / 2 + 7, height - (i * height) / 6);
     }
 
-    return {
-        drawGraph,
-    };
+
+    POINTS.forEach((point, index) => {
+        const x = ((point.x / rValue) * width) / 3 + width / 2;
+        const y = ((-point.y / rValue) * height) / 3 + height / 2;
+
+        ctx.fillStyle = point.color;
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    });
 }
 
+function setActiveButton(parentParameter, element) {
+    const parameterElement = document.getElementById(parentParameter);
+    const buttons = parameterElement.getElementsByTagName("button");
+    for (const button of buttons) {
+        button.className = "";
+    }
+    if (element) {
+        element.className = "active";
+    }
+}
 
-function setY(newValue) {
+function setY(newValue, element) {
     const yButton = document.getElementById("y_value");
     yButton.value = newValue;
+    setActiveButton("y", element);
 }
 
 function setX(newValue) {
@@ -107,12 +139,15 @@ function setX(newValue) {
 
 function setR(newValue) {
     const rButton = document.getElementById("r_value");
-    rButton.value = newValue;
-    rValue = parseFloat(rButton.value);
+    if(rButton){
+        rButton.value = newValue;
+    }
+    rValue = parseFloat(newValue);
+    localStorage.setItem(rCacheKey, newValue);
 }
 
 
-runGrapher().drawGraph();
+drawGraph();
 
 
 canvas.addEventListener('click', (e) => {
