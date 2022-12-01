d5618e0f0701cf07bb3b1913ac187d98fb16a89f
ThisAster <338879@niuitmo.ru>
2022-12-01 20:05:34 +0300

    create class for business logic

---
+++ src/main/java/com/example/demo4/models/Service.java
@@ -0,0 +1,24 @@
+package com.example.demo4.models;
+
+import com.example.demo4.tools.CheckHitManager;
+
+import java.time.LocalDateTime;
+
+public class Service {
+    Triangle triangle = new Triangle();
+    Rectangle rectangle = new Rectangle();
+    Sector sector = new Sector();
+
+    public Point createPoint(Double x, Double y, Integer r, long start) {
+        Point point = new Point();
+
+        point.setX(x);
+        point.setY(y);
+        point.setR(r);
+        point.setProcessTime(System.nanoTime()-start);
+        point.setAttemptTime(LocalDateTime.now());
+        point.setHit(new CheckHitManager(x, y, r).hasHit(sector, rectangle, triangle));
+
+        return point;
+    }
+}
