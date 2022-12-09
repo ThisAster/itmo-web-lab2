e26d611250a52b2e27812aadcfec864716dcdb2d
ThisAster <338879@niuitmo.ru>
2022-12-09 04:05:10 +0300

    fix validation;

--- src/main/java/com/example/demo4/tools/Validation.java
+++ src/main/java/com/example/demo4/tools/Validation.java
@@ -9,13 +9,13 @@ public class Validation {
     private final Integer r_min = 1;
 
     public boolean validate(double x, double y, int r){
-        return validateX(x, x_max, x_min, r) && validateY(y, y_max, y_min, r) && validateR(r, r_max, r_min);
+        return validateX(x, x_max, x_min) && validateY(y, y_max, y_min) && validateR(r, r_max, r_min);
     }
-    private boolean validateX(double x, double x_max, double x_min, int r) {
-        return x >= x_min && x <= x_max && x*x < r*r*1.51f*1.51f;
+    private boolean validateX(double x, double x_max, double x_min) {
+        return x >= x_min && x <= x_max;
     }
-    private boolean validateY(double y, double y_max, double y_min, int r){
-        return y >= y_min && y <= y_max && y*y < r*r*1.51f*1.51f;
+    private boolean validateY(double y, double y_max, double y_min){
+        return y >= y_min && y <= y_max;
     }
     private boolean validateR(int r, int r_max, int r_min){
         return r >= r_min && r <= r_max;
--- src/main/webapp/js/validation.js
+++ src/main/webapp/js/validation.js
@@ -6,29 +6,15 @@ function funcClick() {
     const x = document.getElementById("x").value;
     const y = document.getElementById("y_value").value;
     const r = rValue;
+    const click = document.getElementById("isClick_value").value;
 
-    if (!rValue) {
+    if (!r) {
         alert("Please select a value for R first");
-        return;
-    }
-
-    if(y*y > r*r*1.45*1.45){
-        alert("Y is out of range");
-        return;
-    }
-
-    if(x*x > r*r*1.45*1.45){
-        alert("X is out of range");
         return;
     }
 
     if(!validationFloat(x)) {
         alert("X not Number");
-        document.getElementById("x").value = "";
-        return;
-    }
-    if(!domainFloat(parseFloat(x), -3., 5. )) {
-        alert("X value out of bounds");
         document.getElementById("x").value = "";
         return;
     }
@@ -38,12 +24,21 @@ function funcClick() {
         document.getElementById("x").value = "";
         return;
     }
-    if(!domainFloat(parseFloat(y), -2., 2. )) {
+
+    if (!domainFloat(parseFloat(x), -3., 5. )) {
+        alert("X value out of bounds");
+        document.getElementById("x").value = "";
+        return;
+    }
+
+    if (!domainFloat(parseFloat(y), -2., 2.)) {
         alert("Y value out of bounds");
         document.getElementById("y_value").value = "";
         return;
     }
+
     document.forms["input-form"].submit();
+
 }
 function domainFloat(floatNum, leftBorder, rightBorder) {
     if(floatNum >= rightBorder || floatNum <= leftBorder) {
